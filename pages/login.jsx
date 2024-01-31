5329ce8a41cb29d9549525dff5007e5b8c5cd335
Pedro Ap <pedroaparecidori@gmail.com>
2024-01-31 18:28:24 -0300

    Criado esqueleto da página de login

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,25 +1,45 @@
-import Image from "../components/Image"
-import InputEmail from "../components/InputEmail"
-import InputPassword from "../components/InputPassword"
-import Subtitle from "../components/Subtitle"
-import Button from "../components/Button"
-import Title from "../components/Title"
+import styled from "styled-components"
+
+import Image from "./src/components/layout/Image"
+import InputEmail from "./src/components/InputEmail"
+import InputPassword from "./src/components/InputPassword"
+import Subtitle from "./src/components/typography/Subtitle"
+import Button from "./src/components/Button"
+import Title from "./src/components/typography/Title"
+import Midtitle from "./src/components/typography/Midtitle"
+
+const FormContainer = styled.div`
+  margin-top: 60px;
+
+`
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  margin: 20px 0;
+  gap: 20px;
+`
+
+const Text = styled.p`
+  text-align: center;
+`
 
 function Login () {
   
   return (
-    <div>
-      <div>
-        <Image />
-      </div>
-      <div>
-        <Title>Titulo</Title>
-        <Subtitle>Subtitulo</Subtitle>
-        <InputEmail placeHolder="Email" />
-        <InputPassword placeHolder="Senha" />
-        <Button>Entrar</Button>
-      </div>
-    </div>
+        <Image>
+        <Title># Social Dev</Title>
+        <Subtitle>Tudo que acontece no mundo dev, está aqui!</Subtitle>
+        <FormContainer>
+          <Midtitle>Entre em sua conta</Midtitle>
+        </FormContainer>
+        <Form>
+          <InputEmail placeHolder="Email" />
+          <InputPassword placeHolder="Senha" />
+          <Button>Entrar</Button>
+        </Form>
+        <Text>Não possui uma conta? <a href="#">Faça seu cadastro</a></Text>
+        </Image>
   )
 }
 
---
+++ pages/src/components/typography/Subtitle.jsx
@@ -0,0 +1,8 @@
+import styled from "styled-components"
+
+const Subtitle = styled.h4`
+    font-size: 20px;
+    font-weight: 500;
+`
+
+export default Subtitle
