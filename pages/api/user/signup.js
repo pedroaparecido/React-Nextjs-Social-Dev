083d48b58a80e2396deee5323c2382e43eb3cc5f
Pedro Ap <pedroaparecidori@gmail.com>
2024-02-03 15:37:20 -0300

    Criado serviço de atenticação

--- pages/api/user/signup.js
+++ pages/api/user/signup.js
@@ -1,10 +1,13 @@
 import Joi from "joi"
+import { withIronSessionApiRoute } from 'iron-session/next'
 
 import createHandler from "../../../lib/middlewares/nextConnect"
 
 import validate from "../../../lib/middlewares/validation"
 
 import { signupUser } from "../../../modules/user/user.service"
+
+import { ironConfig } from '../../../lib/middlewares/ironSession'
 
 const postSchema = Joi.object({
     firstName: Joi.string().required().max(50),
@@ -20,11 +23,17 @@ const signup = createHandler()
 signup.post(validate({ body: postSchema }), async (req, res) => {
     try {
         const user = await signupUser(req.body)
-        res.status(200).json(user)
+        req.session.user = {
+            id: user._id,
+            user: user.user    
+        }
+        await req.session.save()
+
+        res.status(200).json({ ok: true })
     } catch (err) {
         console.log(err)
         throw err
     }
 })
 
-export default signup
+export default withIronSessionApiRoute(signup, ironConfig)
